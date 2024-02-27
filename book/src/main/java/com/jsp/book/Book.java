96b9a697868e4bfc1724749c47efb1fea0d1a50c
suraj <surajkumarsharmna440@gmail.com>
2024-02-27 17:21:46 +0530

    book

---
+++ book/src/main/java/com/jsp/book/Book.java
@@ -0,0 +1,50 @@
+package com.jsp.book;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/save")
+public class Book extends HttpServlet{
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	int id= Integer.parseInt(req.getParameter("id"));
+	String name= req.getParameter("name");
+	String author_name= req.getParameter("author_name");
+	int quantity= Integer.parseInt(req.getParameter("quantity"));
+	LocalDate published_date= LocalDate.parse(req.getParameter("published_date")) ;
+		
+	try {
+		Connection connection=new ConnectDatabase().getConnection();
+		PreparedStatement ps=connection.prepareStatement("insert into book values (?,?,?,?,?)");
+		ps.setInt(1, id);
+		ps.setString(2, name);
+		ps.setString(3, author_name);
+		ps.setInt(4, quantity);
+		ps.setObject(5, published_date);
+		int executeUpdate=ps.executeUpdate();
+		PrintWriter writer=resp.getWriter();
+		if(executeUpdate>0) {
+			writer.write("<html><body>");
+			writer.write("<h1>user updated<h1>");
+		}
+		else {
+			writer.write("<html><body>");
+			writer.write("<h1>user not updated<h1>");
+		}
+		connection.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
